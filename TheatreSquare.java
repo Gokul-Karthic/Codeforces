bb89c96a1435b0973fb0259bb22dfc06f233402a
Gokul-Karthic <gokul.23mim10098@vitbhopal.ac.in>
2026-02-08 14:00:44 +0530

    Add files via upload

---
+++ TheatreSquare.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class TheatreSquare {
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            long n = sc.nextInt();
+            long m = sc.nextInt();
+            long a = sc.nextInt();
+            long l = (n + a - 1) / a;
+            long w = (m + a - 1) / a;
+            long total = l * w;
+            System.out.println(total);
+
+      }
+
+}

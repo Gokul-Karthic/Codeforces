f5ee5998090684aaacce242d1c8c876e75fa97ff
Gokul-Karthic <gokulkarthic06@gmail.com>
2026-02-10 21:50:02 +0530

    Add files via upload

---
+++ PetyaandStrings.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PetyaandStrings {
+
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            String s1 = sc.nextLine();
+            String s2 = sc.nextLine();
+
+            // Convert both strings to lowercase
+            s1 = s1.toLowerCase();
+            s2 = s2.toLowerCase();
+
+            // Compare strings
+            int result = s1.compareTo(s2);
+
+            if (result < 0) {
+                  System.out.println("-1");
+            } else if (result > 0) {
+                  System.out.println("1");
+            } else {
+                  System.out.println("0");
+            }
+
+            sc.close();
+      }
+}

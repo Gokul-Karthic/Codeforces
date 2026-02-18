f2dbd7f48a76015242fa2d445a0ef41fcb4e601e
Gokul Karthic <gokulkarthic06@gmail.com>
2026-02-18 21:29:39 +0530

    Add files via upload

---
+++ Presents.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Presents {
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+            int[] p = new int[n + 1];
+            int[] ans = new int[n + 1];
+
+            for (int i = 1; i <= n; i++) {
+                  p[i] = sc.nextInt();
+            }
+
+            for (int i = 1; i <= n; i++) {
+                  ans[p[i]] = i;
+            }
+
+            for (int i = 1; i <= n; i++) {
+                  System.out.print(ans[i] + " ");
+            }
+      }
+}

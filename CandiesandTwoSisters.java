7283503193b7592ec5e1186449c016b54a091a65
Gokul Karthic <gokulkarthic06@gmail.com>
2026-03-29 23:44:26 +0530

    Add files via upload

---
+++ CandiesandTwoSisters.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class CandiesandTwoSisters {
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                  long a = sc.nextLong();
+                  System.out.println((a - 1) / 2);
+
+            }
+      }
+}

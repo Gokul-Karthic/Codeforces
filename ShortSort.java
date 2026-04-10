518955e84a4e715dedae7a7dca2cea17c28d0c27
Gokul Karthic <gokulkarthic06@gmail.com>
2026-04-10 23:09:29 +0530

    Add files via upload

---
+++ ShortSort.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ShortSort {
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+
+            for (int i = 0; i < t; i++) {
+                  String s = sc.next();
+                  if (s.equals("abc") || s.equals("acb") || s.equals("bac") || s.equals("cba")) {
+                        System.out.println("YES");
+                  } else {
+                        System.out.println("NO");
+                  }
+            }
+
+            sc.close();
+      }
+
+}

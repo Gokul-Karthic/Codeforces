0db0cf16515b199bc5d32533e735d22860c8eff7
Gokul Karthic <gokulkarthic06@gmail.com>
2026-04-29 23:42:31 +0530

    Add files via upload

---
+++ Borze.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Borze {
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String s = sc.next();
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+            while (i < s.length()) {
+                  if (s.charAt(i) == '.') {
+                        sb.append('0');
+                        i++;
+                  } else {
+                        if (s.charAt(i) == '-' && s.charAt(i + 1) == '.') {
+                              sb.append('1');
+                              i += 2;
+                        } else {
+                              sb.append('2');
+                              i += 2;
+                        }
+                  }
+            }
+            System.out.println(sb.toString());
+
+      }
+}

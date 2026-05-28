9c54d47b4bb5e0d35feaa1024ae19fecc69fa275
Gokul Karthic <gokulkarthic06@gmail.com>
2026-05-28 23:32:02 +0530

    Add files via upload

---
+++ OddDivisor.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class OddDivisor {
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int t = sc.nextInt();
+
+            while (t-- > 0) {
+                  long n = sc.nextLong();
+
+                  while (n % 2 == 0) {
+                        n = n / 2;
+                  }
+
+                  if (n > 1) {
+                        System.out.println("YES");
+                  } else {
+                        System.out.println("NO");
+                  }
+            }
+
+            sc.close();
+      }
+}

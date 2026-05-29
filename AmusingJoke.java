aa8e6a473ee99b987dbda22c004d34183531da85
Gokul Karthic <gokulkarthic06@gmail.com>
2026-05-29 22:17:25 +0530

    Add files via upload

--- AmusingJoke.java
+++ AmusingJoke.java
@@ -1,29 +1,29 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class AmusingJoke {
       public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
 
-            String guest = sc.nextLine();
-            String host = sc.nextLine();
-            String pile = sc.nextLine();
-            int[] count = new int[26];
-            for (char ch : (guest + host).toCharArray()) {
-                  count[ch - 'A']++;
+            String guest = sc.next();
+            String host = sc.next();
+            String pile = sc.next();
+
+            String combined = guest + host;
+
+            char[] arr1 = combined.toCharArray();
+            char[] arr2 = pile.toCharArray();
+
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+
+            if (Arrays.equals(arr1, arr2)) {
+                  System.out.println("YES");
+            } else {
+                  System.out.println("NO");
             }
 
-            for (char ch : pile.toCharArray()) {
-                  count[ch - 'A']--;
-            }
-
-            for (int i = 0; i < 26; i++) {
-                  if (count[i] != 0) {
-                        System.out.println("NO");
-                        return;
-                  }
-            }
-
-            System.out.println("YES");
+            sc.close();
       }
 
 }

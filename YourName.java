d7ff6cfe6a350252600a7886576202a5b7e5acb5
Gokul Karthic <gokulkarthic06@gmail.com>
2026-06-10 23:47:54 +0530

    Add files via upload

---
+++ YourName.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class YourName {
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int i = sc.nextInt();
+
+            while (i-- > 0) {
+                  int n = sc.nextInt();
+                  String s = sc.next();
+                  String t = sc.next();
+
+                  char[] a = s.toCharArray();
+                  char[] b = t.toCharArray();
+
+                  Arrays.sort(a);
+                  Arrays.sort(b);
+
+                  if (Arrays.equals(a, b)) {
+                        System.out.println("YES");
+                  } else {
+                        System.out.println("NO");
+                  }
+            }
+
+            sc.close();
+      }
+}
